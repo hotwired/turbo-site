2ff23da746357681bd09630cbcae3e12975f653b
Wintereise <Wintereise@users.noreply.github.com>
2022-07-27 08:06:19 +0000

    deploy: 74aa934188f6701ea2356af03981c9898ad95bac

--- _source/_assets/js/main.js
+++
@@ -1,7 +0,0 @@
-import "@hotwired/turbo"
-
-// Are we in an <iframe>?
-if (window.top !== window) {
-  // 🎶 Never Gonna Give You Up 🎶
-  location.replace("https://www.youtube.com/embed/dQw4w9WgXcQ")
-}
